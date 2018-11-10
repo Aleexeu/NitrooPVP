a46d7d37d56128e4b79d7cb459be2a92be875419
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-09 23:53:05 -0200

    Update notificar.js

--- comandos/notificar.js
+++ comandos/notificar.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     var member = message.member.addRole("510619846623363088")
     return message.channel.send("Tag setada com sucesso!");
-
 }
 
 module.exports.help = {
