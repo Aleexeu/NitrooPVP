a82ef5ebdccf9228ffaa1b2aa6d6d60df2c059e5
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-10 08:41:24 -0200

    Update notificar.js

--- comandos/notificar.js
+++ comandos/notificar.js
@@ -1,14 +1,15 @@
 const Discord = require("discord.js");
 
-module.exports.run = async(client, message, args) =>{
-    let roleid = '510619846623363088'
-    let membro = message.mentions.members.first() || message.guild.members.get(args[0])
-    if(!membro) return;
-    let cargo = message.guild.roles.find(c => c.id == `${roleid}`)
-    
-    if(membro.roles.has(roleid)) return message.reply("Esse membro ja possui esse cargo")
-    membro.addRole(roleid).then(() => {message.reply(`Sucesso :minidisc:`)})
+odule.exports.run = (client, message, args) => {
+    var member = message.member.addRole("510619846623363088")
+    return message.channel.send("Tag setada com sucesso!");
+
 }
+
+module.exports.help = {
+  name: "notificar"
+}
+
 
 module.exports.help = {
   name: "notificar"
