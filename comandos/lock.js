e571123527315013cc1d9292eaa975f6b35ff04f
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-15 20:43:25 -0200

    Update lock.js

--- comandos/lock.js
+++ comandos/lock.js
@@ -1,4 +1,4 @@
-const discord = require("Discord.js");
+const Discord = require("discord.js");
 
 module.exports.run = (bot, message, args) => {
     if(!message.guild.member(message.author.id).hasPermissions("MANAGE_ROLES")) return message.reply("<a:BlobBanHammer:471788559402139668> **|** Você não tem permissão para executar este comando!")
