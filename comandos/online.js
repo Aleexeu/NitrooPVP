c4471b86e544c799ff3eabfb6a9f671ba63d0454
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-17 20:56:46 -0200

    Update online.js

--- comandos/online.js
+++ comandos/online.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Infelizmente você não tem permissão!");
     message.channel.send(`Temos ${bot.users.size} membros neste grupo.`)
 };
 
